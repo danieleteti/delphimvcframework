a0198aa2e8a579d7e1e3a3e7bdcb9b440320775e
Daniele Teti <d.teti@bittime.it>
2017-05-11 01:46:17 +0200

    3.0.0 RC3

--- ideexpert/DMVC.Expert.Registration.pas
+++ ideexpert/DMVC.Expert.Registration.pas
@@ -1,52 +1,106 @@
-{***************************************************************************}
-{                                                                           }
-{                      Delphi MVC Framework                                 }
-{                                                                           }
-{     Copyright (c) 2010-2015 Daniele Teti and the DMVCFramework Team       }
-{                                                                           }
-{           https://github.com/danieleteti/delphimvcframework               }
-{                                                                           }
-{***************************************************************************}
-{                                                                           }
-{  Licensed under the Apache License, Version 2.0 (the "License");          }
-{  you may not use this file except in compliance with the License.         }
-{  You may obtain a copy of the License at                                  }
-{                                                                           }
-{      http://www.apache.org/licenses/LICENSE-2.0                           }
-{                                                                           }
-{  Unless required by applicable law or agreed to in writing, software      }
-{  distributed under the License is distributed on an "AS IS" BASIS,        }
-{  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. }
-{  See the License for the specific language governing permissions and      }
-{  limitations under the License.                                           }
-{                                                                           }
-{  This IDE expert is based off of the one included with the DUnitX         }
-{  project.  Original source by Robert Love.  Adapted by Nick Hodges.       }
-{                                                                           }
-{  The DUnitX project is run by Vincent Parrett and can be found at:        }
-{                                                                           }
-{            https://github.com/VSoftTechnologies/DUnitX                    }
-{***************************************************************************}
+// ***************************************************************************
+//
+// Delphi MVC Framework
+//
+// Copyright (c) 2010-2017 Daniele Teti and the DMVCFramework Team
+//
+// https://github.com/danieleteti/delphimvcframework
+//
+// ***************************************************************************
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This IDE expert is based off of the one included with the DUnitX
+// project.  Original source by Robert Love.  Adapted by Nick Hodges.
+//
+// The DUnitX project is run by Vincent Parrett and can be found at:
+//
+// https://github.com/VSoftTechnologies/DUnitX
+// ***************************************************************************
 
 unit DMVC.Expert.Registration;
 
+{.$R DelphiMVCFrameworkSplash.res}
+
 interface
 
-//Note: "Register" method name is case senstive.
+// Note: "Register" method name is case senstive.
 procedure Register;
 
 implementation
 
 uses
   ToolsApi,
+  System.SysUtils,
   Vcl.Dialogs,
   DMVC.Expert.ProjectWizardEx,
-  DMVC.Expert.NewUnitWizardEx;
+  DMVC.Expert.NewUnitWizardEx,
+  Vcl.Graphics,
+  Winapi.Windows;
+
+var
+  AboutBoxServices: IOTAAboutBoxServices = nil;
+  AboutBoxIndex: Integer = 0;
+
+resourcestring
+  resPackageName = 'DelphiMVCFramework';
+  resLicense = 'OpenSource (Apache 2)';
+  resAboutCopyright = 'Copyright Daniele Teti and the DMVCFramework Team';
+  resAboutTitle = 'DelphiMVCFramework';
+  resAboutDescription = 'https://github.com/danieleteti/delphimvcframework';
 
 procedure Register;
 begin
   TDMVCNewProjectWizard.RegisterDMVCProjectWizard(sDelphiPersonality);
   TDMVCNewUnitWizard.RegisterDMVCNewUnitWizard(sDelphiPersonality);
 end;
+
+procedure RegisterSplashScreen;
+var
+  LBmp: Vcl.Graphics.TBitmap;
+begin
+  LBmp := Vcl.Graphics.TBitmap.Create;
+  LBmp.LoadFromResourceName(HInstance, 'SPLASH');
+  SplashScreenServices.AddPluginBitmap(resPackageName, LBmp.Handle, False, resLicense, '');
+  LBmp.Free;
+end;
+
+procedure RegisterAboutBox;
+var
+  LProductImage: HBITMAP;
+begin
+  Supports(BorlandIDEServices, IOTAAboutBoxServices, AboutBoxServices);
+  LProductImage := LoadBitmap(FindResourceHInstance(HInstance), 'SPLASH');
+  AboutBoxIndex := AboutBoxServices.AddPluginInfo(resPackageName, resAboutDescription, LProductImage, False, resLicense);
+end;
+
+procedure UnregisterAboutBox;
+begin
+  if (AboutBoxIndex = 0) and Assigned(AboutBoxServices) then
+  begin
+    AboutBoxServices.RemovePluginInfo(AboutBoxIndex);
+    AboutBoxIndex := 0;
+    AboutBoxServices := nil;
+  end;
+end;
+
+initialization
+
+// RegisterSplashScreen;
+// RegisterAboutBox;
+
+finalization
+
+// UnregisterAboutBox;
 
 end.
