cd77144a2b0bbf48ff1d7fdca73307c4d28ab897
joao.duarte <joao.antonioduarte@hotmail.com>
2018-11-22 12:00:43 -0200

    Testing RQL Parser PostgreSQL

--- tools/rql2sql/RQL2SQL.dpr
+++ tools/rql2sql/RQL2SQL.dpr
@@ -6,7 +6,8 @@ uses
   MVCFramework.RQL.AST2FirebirdSQL in '..\..\sources\MVCFramework.RQL.AST2FirebirdSQL.pas',
   MVCFramework.RQL.Parser in '..\..\sources\MVCFramework.RQL.Parser.pas',
   MVCFramework.RQL.AST2MySQL in '..\..\sources\MVCFramework.RQL.AST2MySQL.pas',
-  MVCFramework.RQL.AST2InterbaseSQL in '..\..\sources\MVCFramework.RQL.AST2InterbaseSQL.pas';
+  MVCFramework.RQL.AST2InterbaseSQL in '..\..\sources\MVCFramework.RQL.AST2InterbaseSQL.pas',
+  MVCFramework.RQL.AST2PostgreSQL in '..\..\sources\MVCFramework.RQL.AST2PostgreSQL.pas';
 
 {$R *.res}
 
