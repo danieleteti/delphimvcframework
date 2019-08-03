47029fb1a7aa43dca06098f2a135977e73d8bce9
Geoffrey Smith <github@tysontrechnology.com.au>
2019-08-03 15:15:24 +1000

    Update of Generated code sample

--- samples/SwaggerSampleApi/mvccontrollerclient.pas
+++ samples/SwaggerSampleApi/mvccontrollerclient.pas
@@ -8,99 +8,125 @@ uses
   , REST.Client
   , REST.Authenticator.OAuth
   , REST.Types
-  , Generics.Collections
   , MVCFramework
   , MVCFramework.Commons
   ;
 
 
 (*
-Title: Sample API
-Description: Sample API Description
-License: Apache License - Version 2.0, January 2004
+Title: Swagger Petstore
+Description: This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.
+License: Apache 2.0
 *)
 
 type
-  TAddress = class
-    [MVCDoc('The employee address description.')]
-    description : string;
+  notdefined = string;
 
-    [MVCDoc('The employee address city.')]
-    city : string;
-
-    [MVCDoc('The employee address region.')]
-    region : string;
-
-    [MVCDoc('The employee address country.')]
-    country : string;
-
-    [MVCDoc('The employee address postal code.')]
-    postalCode : string;
-
-  end;
-
-  TEmployee = class
-    [MVCDoc('The employee full name.')]
-    [MVCMaxLength(80)]
-    name : string;
-
-    [MVCDoc('The employee ID Number.')]
-    [MVCFormat('int64')]
-    [MVCMinimum(0)]
-    [MVCMaximum(99999999)]
-    empId : integer;
-
-    [MVCDoc('The employee phone number.')]
-    phone : string;
-
-    [MVCDoc('The employee hire date.')]
-    [MVCFormat('yyyy-MM-dd')]
-    hireDate : string;
-
-    [MVCDoc('The employee gross salary.')]
-    salary : Double;
-
-    [MVCDoc('The employee full address.')]
-    address : TAddress;
-
-  end;
-
-  [MVCPath('/api')]
+  [MVCPath('/v2')]
   TMyMVCControllerClient = class(TObject)
     RESTClient : TRESTClient;
-
     RESTRequest : TRESTRequest;
-
     RESTResponse : TRESTResponse;
 
-    [MVCDoc('Returns a employee list.')]
-    [MVCPath('/employees')]
-    [MVCHTTPMethod([httpget])]
-    [MVCResponseList(200, 'Successfully returns data', TEmployee)]
-    procedure GetEmployeeList;
-
-    [MVCDoc('Creates a employee.')]
-    [MVCPath('/employees')]
+    [MVCDoc('')]
+    [MVCPath('/pet')]
     [MVCHTTPMethod([httppost])]
-    [MVCResponse(201, 'Successfully creates data', TEmployee)]
-    procedure CreateEmployee(paramEmployee: TEmployee);
+    procedure addPet(paramBody: notdefined);
 
-    [MVCDoc('Returns a employee.')]
-    [MVCPath('/employees/($id)')]
-    [MVCHTTPMethod([httpget])]
-    [MVCResponse(200, 'Successfully returns data', TEmployee)]
-    procedure GetEmployee(paramid: Integer);
-
-    [MVCDoc('Updates a employee.')]
-    [MVCPath('/employees/($id)')]
+    [MVCDoc('')]
+    [MVCPath('/pet')]
     [MVCHTTPMethod([httpput])]
-    [MVCResponse(200, 'Successfully updates data', TEmployee)]
-    procedure UpdateEmployee(paramid: Integer; paramEmployee: TEmployee);
+    procedure updatePet(paramBody: notdefined);
 
-    [MVCDoc('Deletes a employee.')]
-    [MVCPath('/employees/($id)')]
+    [MVCDoc('Multiple status values can be provided with comma separated strings')]
+    [MVCPath('/pet/findByStatus')]
+    [MVCHTTPMethod([httpget])]
+    procedure findPetsByStatus(paramStatus: Array of string);
+
+    [MVCDoc('Multiple tags can be provided with comma separated strings. Use tag1, tag2, tag3 for testing.')]
+    [MVCPath('/pet/findByTags')]
+    [MVCHTTPMethod([httpget])]
+    procedure findPetsByTags(paramTags: Array of string);
+
+    [MVCDoc('Returns a single pet')]
+    [MVCPath('/pet/{petId}')]
+    [MVCHTTPMethod([httpget])]
+    procedure getPetById(paramPetId: Integer);
+
+    [MVCDoc('')]
+    [MVCPath('/pet/{petId}')]
+    [MVCHTTPMethod([httppost])]
+    procedure updatePetWithForm(paramPetId: Integer; paramName: String; paramStatus: String);
+
+    [MVCDoc('')]
+    [MVCPath('/pet/{petId}')]
     [MVCHTTPMethod([httpdelete])]
-    procedure DeleteEmployee;
+    procedure deletePet(paramApi_key: String; paramPetId: Integer);
+
+    [MVCDoc('')]
+    [MVCPath('/pet/{petId}/uploadImage')]
+    [MVCHTTPMethod([httppost])]
+    procedure uploadFile(paramPetId: Integer; paramAdditionalMetadata: String; paramFile: string);
+
+    [MVCDoc('Returns a map of status codes to quantities')]
+    [MVCPath('/store/inventory')]
+    [MVCHTTPMethod([httpget])]
+    procedure getInventory;
+
+    [MVCDoc('')]
+    [MVCPath('/store/order')]
+    [MVCHTTPMethod([httppost])]
+    procedure placeOrder(paramBody: notdefined);
+
+    [MVCDoc('For valid response try integer IDs with value >= 1 and <= 10. Other values will generated exceptions')]
+    [MVCPath('/store/order/{orderId}')]
+    [MVCHTTPMethod([httpget])]
+    procedure getOrderById(paramOrderId: Integer);
+
+    [MVCDoc('For valid response try integer IDs with positive integer value. Negative or non-integer values will generate API errors')]
+    [MVCPath('/store/order/{orderId}')]
+    [MVCHTTPMethod([httpdelete])]
+    procedure deleteOrder(paramOrderId: Integer);
+
+    [MVCDoc('This can only be done by the logged in user.')]
+    [MVCPath('/user')]
+    [MVCHTTPMethod([httppost])]
+    procedure createUser(paramBody: notdefined);
+
+    [MVCDoc('')]
+    [MVCPath('/user/createWithArray')]
+    [MVCHTTPMethod([httppost])]
+    procedure createUsersWithArrayInput(paramBody: notdefined);
+
+    [MVCDoc('')]
+    [MVCPath('/user/createWithList')]
+    [MVCHTTPMethod([httppost])]
+    procedure createUsersWithListInput(paramBody: notdefined);
+
+    [MVCDoc('')]
+    [MVCPath('/user/login')]
+    [MVCHTTPMethod([httpget])]
+    procedure loginUser(paramUsername: String; paramPassword: String);
+
+    [MVCDoc('')]
+    [MVCPath('/user/logout')]
+    [MVCHTTPMethod([httpget])]
+    procedure logoutUser;
+
+    [MVCDoc('')]
+    [MVCPath('/user/{username}')]
+    [MVCHTTPMethod([httpget])]
+    procedure getUserByName(paramUsername: String);
+
+    [MVCDoc('This can only be done by the logged in user.')]
+    [MVCPath('/user/{username}')]
+    [MVCHTTPMethod([httpput])]
+    procedure updateUser(paramUsername: String; paramBody: notdefined);
+
+    [MVCDoc('This can only be done by the logged in user.')]
+    [MVCPath('/user/{username}')]
+    [MVCHTTPMethod([httpdelete])]
+    procedure deleteUser(paramUsername: String);
 
   end;
 
@@ -114,39 +140,102 @@ uses
 
 
 
-procedure TMyMVCControllerClient.GetEmployeeList;
-var
-  employee : TObjectList<TEmployee>;
+procedure TMyMVCControllerClient.addPet(paramBody: notdefined);
 begin
-  employee := TObjectList<TEmployee>.Create;
 
 end;
 
-procedure TMyMVCControllerClient.CreateEmployee(paramEmployee: TEmployee);
-var
-  employee : TEmployee;
+procedure TMyMVCControllerClient.updatePet(paramBody: notdefined);
 begin
-  employee := TEmployee.Create;
 
 end;
 
-procedure TMyMVCControllerClient.GetEmployee(paramid: Integer);
-var
-  employee : TEmployee;
+procedure TMyMVCControllerClient.findPetsByStatus(paramStatus: Array of string);
 begin
-  employee := TEmployee.Create;
 
 end;
 
-procedure TMyMVCControllerClient.UpdateEmployee(paramid: Integer; paramEmployee: TEmployee);
-var
-  employee : TEmployee;
+procedure TMyMVCControllerClient.findPetsByTags(paramTags: Array of string);
 begin
-  employee := TEmployee.Create;
 
 end;
 
-procedure TMyMVCControllerClient.DeleteEmployee;
+procedure TMyMVCControllerClient.getPetById(paramPetId: Integer);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.updatePetWithForm(paramPetId: Integer; paramName: String; paramStatus: String);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.deletePet(paramApi_key: String; paramPetId: Integer);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.uploadFile(paramPetId: Integer; paramAdditionalMetadata: String; paramFile: string);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.getInventory;
+begin
+
+end;
+
+procedure TMyMVCControllerClient.placeOrder(paramBody: notdefined);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.getOrderById(paramOrderId: Integer);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.deleteOrder(paramOrderId: Integer);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.createUser(paramBody: notdefined);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.createUsersWithArrayInput(paramBody: notdefined);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.createUsersWithListInput(paramBody: notdefined);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.loginUser(paramUsername: String; paramPassword: String);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.logoutUser;
+begin
+
+end;
+
+procedure TMyMVCControllerClient.getUserByName(paramUsername: String);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.updateUser(paramUsername: String; paramBody: notdefined);
+begin
+
+end;
+
+procedure TMyMVCControllerClient.deleteUser(paramUsername: String);
 begin
 
 end;
