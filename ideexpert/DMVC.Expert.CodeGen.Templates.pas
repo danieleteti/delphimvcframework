ff66bddc69b7cca2ccbf11939a4caccb4fe8648b
Daniele Teti <d.teti@bittime.it>
2023-10-04 23:13:49 +0200

    Wizard generates support for Profiler configured from .env

--- ideexpert/DMVC.Expert.CodeGen.Templates.pas
+++ ideexpert/DMVC.Expert.CodeGen.Templates.pas
@@ -117,6 +117,13 @@ resourcestring
     '      end);' + sLineBreak +
     '' + sLineBreak +
     '    WebRequestHandlerProc.MaxConnections := dotEnv.Env(''dmvc.handler.max_connections'', 1024);' + sLineBreak +	
+	'' + sLineBreak +	
+    '    if dotEnv.Env(''dmvc.profiler.enabled'', false) then' + sLineBreak +	
+    '    begin' + sLineBreak +	
+    '      Profiler.ProfileLogger := Log;' + sLineBreak +	
+    '      Profiler.WarningThreshold := dotEnv.Env(''dmvc.profiler.warning_threshold'', 2000);' + sLineBreak +	
+    '    end;' + sLineBreak +	
+	'' + sLineBreak +				
     '    RunServer(dotEnv.Env(''dmvc.server.port'', %1:d));' + sLineBreak +
     '  except' + sLineBreak +
     '    on E: Exception do' + sLineBreak +
