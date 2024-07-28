641b89cf366369ede29f82c655b646fb2cc92262
Daniele Teti <d.teti@bittime.it>
2024-07-28 23:30:23 +0200

    New demo instant_search_with_htmx

---
+++ samples/instant_search_with_htmx/Controllers.BooksU.pas
@@ -0,0 +1,63 @@
+unit Controllers.BooksU;
+
+interface
+
+uses
+  MVCFramework, MVCFramework.Commons, MVCFramework.Serializer.Commons, System.Generics.Collections;
+
+type
+  [MVCPath]
+  TBooksController = class(TMVCController)
+  public
+    [MVCPath]
+    [MVCPath('/search')]
+    function Search([MVCFromQueryString('query','')] SearchQueryText: String): String;
+  end;
+
+implementation
+
+uses
+  System.StrUtils, System.SysUtils, MVCFramework.Logger,
+  MVCFramework.ActiveRecord,
+  MVCFramework.HTMX,
+  MVCFramework.Middleware.ActiveRecord,
+  Data.DB;
+
+
+{ TBooksController }
+
+function TBooksController.Search(SearchQueryText: String): String;
+var
+  lDS: TDataSet;
+  lBaseSelect, lOrdering: String;
+begin
+  SearchQueryText := SearchQueryText.ToLower;
+  lBaseSelect := 'select id, book_name, author_name, genre from books';
+  lOrdering := 'order by book_name COLLATE NOCASE';
+  if SearchQueryText.IsEmpty then
+  begin
+    lDS := TMVCActiveRecord.SelectDataSet(lBaseSelect + ' ' + lOrdering, [], True);
+  end
+  else
+  begin
+    lDS := TMVCActiveRecord.SelectDataSet(
+      lBaseSelect + ' where instr(lower(book_name), ?) > 0 or instr(lower(author_name), ?) > 0 or instr(lower(genre), ?) > 0 ' + lOrdering,
+      [SearchQueryText, SearchQueryText, SearchQueryText], True);
+  end;
+  try
+    ViewData['books'] := lDS;
+    ViewData['books_count'] := lDS.RecordCount;
+    if Context.Request.IsHTMX then
+    begin
+      Result := PageFragment(['search_results']);
+    end
+    else
+    begin
+      Result := Page(['index']);
+    end;
+  finally
+    lDS.Free;
+  end;
+end;
+
+end.
---
+++ samples/instant_search_with_htmx/WebModuleU.dfm
@@ -0,0 +1,11 @@
+object MyWebModule: TMyWebModule
+  OnCreate = WebModuleCreate
+  OnDestroy = WebModuleDestroy
+  Actions = <>
+  Height = 230
+  Width = 415
+  object FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink
+    Left = 192
+    Top = 96
+  end
+end
