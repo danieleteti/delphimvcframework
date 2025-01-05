98c13ae5beca866045f65b0a8e2295ebe5ec144b
Marcelo Jaloto <marcelojaloto@gmail.com>
2025-01-05 20:22:38 -0300

    Fixes rendering in Swagger documentation for TArray type fields.

--- sources/MVCFramework.Swagger.Commons.pas
+++ sources/MVCFramework.Swagger.Commons.pas
@@ -637,8 +637,19 @@ begin
     begin
       lJsonFieldClass := GetJsonFieldClass(TypeKindToMVCSwagSchemaType(
         TRttiDynamicArrayType(lProp.PropertyType).ElementType));
+
       if Assigned(lJsonFieldClass) then
-        TJsonFieldArray(lJsonField).ItemFieldType := lJsonFieldClass.Create;
+      begin
+        if lJsonFieldClass.InheritsFrom(TJsonFieldObject) then
+        begin
+          lClass := TRttiDynamicArrayType(lProp.PropertyType).ElementType.AsInstance.MetaclassType;
+          lJsonFieldObject := TJsonFieldObject.Create;
+          ExtractJsonSchemaFromClass(lJsonFieldObject, lClass, aHTTPMethod, aIsResponseSchema);
+          TJsonFieldArray(lJsonField).ItemFieldType := lJsonFieldObject;
+        end
+        else
+          TJsonFieldArray(lJsonField).ItemFieldType := lJsonFieldClass.Create;
+      end;
     end
     else if lJsonField is TJsonFieldEnum then /// Extract enumerator information
     begin
